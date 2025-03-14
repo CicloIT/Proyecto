6d5203423169f1f65b663e2e37d0eaa92746770a
cicloIT <desarrollo@cicloit.com>
2025-03-14 14:11:07 -0300

    .

--- Backend/server.js
+++ Backend/server.js
@@ -7,11 +7,14 @@ const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
 
-
+const db = createClient({
+  url: 'libsql://cicloit-samuelnar.turso.io',
+  authToken: process.env.TURSO_DB_TOKEN,
+});
 
 app.get("/items", async (req, res) => {
   try {
-    const result = await db.client.execute("SELECT * FROM items");
+    const result = await db.execute("SELECT * FROM items");
     res.json(result.rows);
   } catch (error) {
     res.status(500).json({ error: error.message });
@@ -21,7 +24,7 @@ app.get("/items", async (req, res) => {
 app.post("/items", async (req, res) => {
   const { name, category } = req.body;
   try {
-    await db.client.execute("INSERT INTO items (name, category) VALUES (?, ?)", [name, category]);
+    await db.execute("INSERT INTO items (name, category) VALUES (?, ?)", [name, category]);
     res.status(201).json({ message: "Item added" });
   } catch (error) {
     res.status(500).json({ error: error.message });
